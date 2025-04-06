b776e3b36f0ae29f687c4acd2c46ab2c84cc0d5d
Boris Ruskov <boris@Boriss-Macbook-Pro.local>
2025-04-06 03:27:28 +0300

    Adds LobbyManager

--- src/service/ChatRoom.ts
+++ src/service/ChatRoom.ts
@@ -1,29 +1,69 @@
-// import Stream from "node:stream";
-import Participant, { ParticipantId } from "./Participant";
+import Participant from "./Participant";
 import { IMessage } from "../main";
+import { EventEmitter } from "node:stream";
 
-export default class ChatRoom {
+export default class ChatRoom extends EventEmitter {
   private name: string;
   private participants: Set<Participant>;
 
   constructor(roomName: string, participants?: Set<Participant>) {
+    super();
     this.name = roomName;
     this.participants = participants ? participants : new Set<Participant>();
+
+    this.on("user_subscribed", () => {});
+
+    this.on("user_unsubscribed", (p: Participant) => {
+      this.participants.delete(p);
+    });
+
+    this.on("user_subscribed", () => {});
+
+    this.on("user_disconnected", () => {});
   }
 
   subscribe = (participant: Participant): void => {
+    Log?.debug(`Participant ${participant.name} has joined ${this.name} romm.`);
+
+    // Setup up WS listenres
+    participant.ws.on("message", (data) => {
+      this.emit<IMessage>("user_subscribed", {});
+    });
+
+    participant.ws.on("close", (data) => {
+      // this.disconnectParticipant(p);
+    });
+
+    participant.ws.on("pong", (data) => {
+      this.emit<IMessage>("pong", {});
+    });
+
     this.participants.add(participant);
   };
 
-  unsubscribe = (participant: Participant) => {
-    this.participants.delete(participant);
+  unsubscribe = (p: Participant) => {
+    Log?.debug(`Participant ${p.name} has left ${this.name} romm.`);
+
+    this.participants.delete(p);
+    this.emit("user_unsubscribed", {});
   };
 
-  fanout = (message: IMessage) => {
+  disconnectParticipant = (p: Participant) => {
+    if (this.participants.has(p)) {
+      this.emit("user_unsubscribed");
+      let msg: IMessage = {
+        topic: "System",
+        action: "subscribe",
+        data: "",
+        origin: "string",
+      };
+      this.broadcast(msg);
+    }
+  };
+
+  broadcast = (message: IMessage) => {
     for (const participant of this.participants) {
       participant.ws.send(JSON.stringify(message));
     }
   };
-
-  publish = (): void => {};
 }
---
+++ src/service/LobbyManager.ts
@@ -0,0 +1,47 @@
+import { EventEmitter } from "stream";
+import ChatRoom from "./ChatRoom";
+import Participant from "./Participant";
+
+export default class LobbyManager extends EventEmitter {
+  rooms: Set<ChatRoom>;
+
+  constructor(
+    chatRooms: ChatRoom[] = [
+      new ChatRoom("General"),
+      new ChatRoom("News"),
+      new ChatRoom("Q&A"),
+    ]
+  ) {
+    super();
+    this.rooms = new Set<ChatRoom>();
+    chatRooms.forEach((r: ChatRoom) => {
+      this.rooms.add(r);
+      this.setRoomListeners(r);
+    });
+
+    this.on("user_disconnected", (p: Participant) => {
+      for (const room of p.ws.subscribedTo)
+        room.emit("user_unsubscribed", () => {});
+    });
+  }
+
+  setRoomListeners(r: ChatRoom) {
+    r.on("user_subscribed", () => {});
+    r.on("user_unsubscribed", () => {});
+    r.on("user_disconnected", () => {
+      this.emit("user_disconnected");
+    });
+  }
+
+  createRoom = (r: ChatRoom) => {
+    this.rooms.add(r);
+  };
+
+  disposeRoom = (r: ChatRoom) => {
+    this.rooms.delete(r);
+  };
+
+  addUserToLobby = (p: Participant) => {
+    p.ws.on("message", () => {});
+  };
+}
